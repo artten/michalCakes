a2fafa3faf6decb83e92010ce500700df34e1903
Artiom Divak <artten12380@gmail.com>
2022-01-13 18:38:30 +0200

    deploy

--- src/components/CakeGallery.js
+++ src/components/CakeGallery.js
@@ -6,20 +6,7 @@ import CakeCard from './CakeCard';
 const CakeGallery = () => {
   return(
     <>
-            <div className="container mt-5">
-      <div className="row align-items-center">
-          <div className="col-4 mx-auto">
-          <CakeCard name= "banana" image= {img}/>
-          </div>
-          <div className="col-4 mx-auto">
-          <CakeCard name= "banana" image= {img}/>
-          </div>
-          <div className="col-4 mx-auto">
-          <CakeCard name= "banana" image= {img}/>
-          </div>
-        </div>
-        
-    </div>
+
     </>
   )
 }
