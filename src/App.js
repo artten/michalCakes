36540b193aadb09a85ee55a06bf30e8c075d35a0
Artiom Divak <artten12380@gmail.com>
2022-01-15 11:27:08 +0200

    + - buttons added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import CakeCard from './components/CakeCard';
-// import Banana_Cake from "./images/Banana_Cake.jpeg"
-// import O_Letter_Cake from "./images/O_Letter_Cake.jpeg"
-// import E_Letter_Cake from "./images/E_Letter_Cake.jpeg"
-// import A_Letter_Cake from "./images/A_Letter_Cake.jpeg"
-// import Honey_Moon from "./images/Honey_Moon_Cake.jpeg"
-// import Shmarim_Cake1 from "./images/Shmarim_Cake.jpeg"
-// import Shmarim_Cake2 from "./images/Shmarim_Cake(2).jpeg"
-// import Strawberry_Cake from "./images/Strawberry_Cake.jpeg"
 import CakesDB from "./CakesDatabase.json"
+import banana from "./images/Banana_Cake.jpeg"
 
 
 function App() {
@@ -25,7 +18,7 @@ function App() {
         {
             CakesDB && CakesDB.map((item) => (
               <div key={item.id} className='card'>
-               <CakeCard name = {item.name} price = {item.price} pic={item.image}/>
+               <CakeCard name = {item.name} price = {item.price} pic={process.env.PUBLIC_URL + "/" + item.image}/>
                 </div>
               ))
         }
--- src/components/CakeCard.js
+++ src/components/CakeCard.js
@@ -1,8 +1,21 @@
 
-import { Button,Card } from 'react-bootstrap';
+import { Button,Card,ButtonGroup } from 'react-bootstrap';
+import React, { useState } from "react";
+
 
 const CakeCard = ({name,price,pic}) => {
-    console.log(pic)
+  let [item_count, setCount] = useState(0);
+  let [spaces, setSpaces] = useState("   ");
+  function increase() {
+    item_count++;
+    setCount(item_count)
+  }
+  function decrease() {
+    if (item_count != 0) {
+      item_count--;
+      setCount(item_count)
+    }
+  }
     return (
       <Card className='text-center' style={{ width: '18rem' }}>
         <Card.Img variant="top" src={pic}/>
@@ -11,7 +24,20 @@ const CakeCard = ({name,price,pic}) => {
           <Card.Text>
             {price}$
           </Card.Text>
-          <Button variant="primary">Buy</Button>
+          {/* <ButtonGroup vertical> */}
+            
+            <ButtonGroup className="me-3" aria-label="First group">
+            <Button variant="primary" onClick={increase} >+</Button>
+            <Card.Text style={{ marginLeft: '.7rem', marginRight: '.7rem' }} >{item_count}</Card.Text>
+            <Button variant="primary"  onClick={decrease}>-</Button>
+            </ButtonGroup>
+            <br/>
+            <br/>
+            <ButtonGroup className="me-3" aria-label="second group">
+            <Button variant="primary">Buy</Button>
+          </ButtonGroup>
+
+          {/* </ButtonGroup> */}
         </Card.Body>
       </Card>
     )
